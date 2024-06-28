cf3d525b5d97bb6f391bf524cea4d909a9ba9859
Alexey <nafiguspofigus@gmail.com>
2024-06-28 13:28:46 +0300

    Update cs_45.c

--- cs_45.c
+++ cs_45.c
@@ -7,9 +7,11 @@
 const int Biase = BITS - 1;
 char number_in_bits[BITS];
 
-void print_header(); //print header
 int multiplyer(int);
+void print_header();
 void bits_maker(int);
+int power_of_two(int);
+int number_multyplayer(char*, int, int);
 
 int main()
 {
@@ -18,10 +20,10 @@ int main()
     int number_of_variants = multiplyer(Biase); // number of variants
     for(int i = 0; i < number_of_variants; i++) {
         bits_maker(i);
-        printf("\t%s\n", number_in_bits);
-        
+        printf(" %s", number_in_bits);
+        printf(" \t\t%d", number_multyplayer(number_in_bits, 1, 2));
+        printf("\n");
     }
-
     return 0;
 }
 
@@ -35,6 +37,22 @@ void bits_maker(int number) {
         number_in_bits[BITS - 1 - i] = (number & 1) + '0';
         number = number >> 1;
     }
+}
+
+
+int number_multyplayer(char* arr, int position, int count) {
+    int result = 0;
+    for(int i = count - 1, j = 0; i >= 0; i--, j++) {
+        result += (arr[position + i] - '0') * power_of_two(j);
+    }
+}
+
+int power_of_two(int numb){
+    int result = 1;
+    for(int i = 0; i < numb; i++){
+        result = result * 2;
+    }
+    return result;
 }
 
 int multiplyer(int power_of_two) {
